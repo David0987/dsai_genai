7d01846273a14ece1efc9c001df8b08b286f92b2
david <david.hhs@gmail.com>
2025-05-13 20:11:58 +0800

    helloworld

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    # if request.method == "POST":
+    #     t = request.form.get("txt")
+    #     r = palm.generate_test(**defaults,messages=t)
+    #     return(render_template("index.html",result=r.last))
+    # else:
+    #     return(render_template("index.html",result="waiting"))
+    return(render_template("index.html"))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001, debug=True)
