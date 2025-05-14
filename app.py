574e8f623aaac225e96be41480e4fbf709517847
david <david.hhs@gmail.com>
2025-05-14 10:06:50 +0800

    working with gemini

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
+import google.generativeai as genai
+# from google.generativeai import 
+
+# import google.generativeai as genai
+gemini_api = 'AIzaSyBf_C_bV9mPzvwOkwI9Hqzyxx_6QJdPYc8'
+genai.configure(api_key=gemini_api)
 
 app = Flask(__name__)
 
@@ -20,9 +26,15 @@ def gemini():
 def gemini_reply():
     q = request.form.get("q")
     print('q', q)
-    #gemini
-    r = q
+    model = genai.GenerativeModel('gemini-1.5-flash-8b')
+    # Initialize the model
+
+    # r = model.generate_content(q)
+    r = model.generate_content(q)
+
+    r = r.text
+#
     return(render_template("gemini_reply.html",r=r))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5002, debug=True)
