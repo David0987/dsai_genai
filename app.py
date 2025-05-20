2a529770ed8875b3271f6c1c2718558f2279e87a
david <david.hhs@gmail.com>
2025-05-20 12:50:41 +0800

    working version

--- app.py
+++ app.py
@@ -2,14 +2,18 @@ from flask import Flask, render_template, request, redirect, url_for
 import google.generativeai as genai
 from config import Config
 import os
+import sqlite3
+import datetime
 
 gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+# conn = sqlite3.connect('user.db')
+# conn.execute('create table users (name text, timestamp timestamp)')
 
 # Configure the Gemini API with the key from Config
 genai.configure(api_key=gemini_api_key)
 genai.configure(api_key=Config.GEMINI_API_KEY)
 model = genai.GenerativeModel('gemini-1.5-flash-8b')
-
 
 app = Flask(__name__)
 
@@ -17,9 +21,51 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route("/main",methods=["GET","POST"])
+def main():
+    if request.method == "POST":
+        q = request.form.get("q")
+        print("where is the q",q)
+        t = datetime.datetime.now()
+        
+        conn = sqlite3.connect('user.db')
+        c = conn.cursor()
+        c.execute("insert into users(name,timestamp) values(?,?)",(q,t))
+        conn.commit()
+        c.close()
+        conn.close()
+    return(render_template("main.html"))
+
 @app.route("/gemini",methods=["GET","POST"])
 def gemini():
     return(render_template("gemini.html"))
+
+@app.route("/user_log",methods=["GET","POST"])
+def user_log():
+    conn = sqlite3.connect('user.db')
+    c = conn.cursor()
+    c.execute("select * from users")
+    r = ""
+    for row in c:
+        print(row)
+        r = r+str(row) + "\n"
+        print(r)
+    c.close()
+    print("r: ",r)
+    conn.close()
+
+    return(render_template("user_log.html",r=r))
+
+@app.route("/delete_log",methods=["GET","POST"])
+def delete_log():
+    conn = sqlite3.connect('user.db')
+    c = conn.cursor()
+    c.execute("delete from users")
+    # r = ""
+    conn.commit()
+    c.close()
+    conn.close()
+    return(render_template("delete_log.html"))
 
 @app.route("/gemini_reply",methods=["GET","POST"])
 def gemini_reply():
@@ -33,4 +79,4 @@ def gemini_reply():
 
 if __name__ == "__main__":
     # app.run(host=Config.HOST, port=Config.PORT, debug=Config.DEBUG)
-    app.run()
+    app.run(debug=True)
